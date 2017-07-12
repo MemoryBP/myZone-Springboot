363e33b274dd78e9be8cf4181f2305c12297564e
memorybp <809558630@qq.com>
2017-07-12 14:04:19 +0800

    默认

---
+++ src/main/java/com/myzone/model/TaskWeekly.java
@@ -0,0 +1,38 @@
+package com.myzone.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name="taskWeekly")
+public class TaskWeekly implements Serializable{
+    
+
+	/**
+	 * 
+	 */
+	@Transient
+	private static final long serialVersionUID = 1252148870446365282L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)//不创建hibernate_sequence表 // 自动生成,当你插入时会自动更新idOK??? 恩
+	@Column(length = 20)
+	private Long id;
+	
+	@Column(length = 255)
+    private String event;
+	
+	@Column(length = 20,nullable=false)
+    private String cron;
+
+    @Column(length = 255)
+    private String template;
+
+	@Column
+    private Date createDate;
+	
+	@Column
+    private Date updateDate;
+
+}
--- src/main/java/com/myzone/util/QuartzJob.java
+++ src/main/java/com/myzone/util/QuartzJob.java
@@ -3,6 +3,7 @@ package com.myzone.util;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.text.MessageFormat;
 import java.text.SimpleDateFormat;
 
 /**
@@ -10,6 +11,20 @@ import java.text.SimpleDateFormat;
  */
 @Component
 public class QuartzJob {
+    @Scheduled(cron="0 0 16 ? * FRI")//周五的下午16:00触发 生成数据统计周报
+    public void weekly() {
+
+        //获取统计数据值
+
+        //配置短信内容
+
+        //配置短信接口参数
+
+        //启动短信接口
+        String time = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(System.currentTimeMillis());
+        System.out.println(MessageFormat.format("周报已发送 {0}",time));
+    }
+
     @Scheduled(cron="0 0 9 ? * MON-FRI")//周一至周五的上午9:00触发
     public void toWork() {
         /*String time = new SimpleDateFormat("MMM d，yyyy KK:mm:ss a", Locale.ENGLISH).format(System.currentTimeMillis());*/
