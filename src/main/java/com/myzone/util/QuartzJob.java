bfcd7f2c9fd921369a0da7acff66d0b8d56821d2
memorybp <809558630@qq.com>
2017-07-12 13:09:51 +0800

    设置Quartz注解方式

--- src/main/java/com/myzone/util/QuartzJob.java
+++ src/main/java/com/myzone/util/QuartzJob.java
@@ -1,14 +1,45 @@
 package com.myzone.util;
 
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
 import java.text.SimpleDateFormat;
-import java.util.Locale;
 
 /**
  * Created by 陈 on 2017/7/11.
  */
+@Component
 public class QuartzJob {
-    public void work() {
-        String time = new SimpleDateFormat("MMM d，yyyy KK:mm:ss a", Locale.ENGLISH).format(System.currentTimeMillis());
+    @Scheduled(cron="0 0 12 ? * *")
+    public void eatLunch() {
+        /*String time = new SimpleDateFormat("MMM d，yyyy KK:mm:ss a", Locale.ENGLISH).format(System.currentTimeMillis());*/
+        String time = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(System.currentTimeMillis());
         System.out.println("time:"+time);
+        System.out.println("吃饭啦!!!!!!!!!!!");
     }
+
+    @Scheduled(cron="0 0 13 ? * *")
+    public void workAfternoon() {
+        /*String time = new SimpleDateFormat("MMM d，yyyy KK:mm:ss a", Locale.ENGLISH).format(System.currentTimeMillis());*/
+        String time = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(System.currentTimeMillis());
+        System.out.println("time:"+time);
+        System.out.println("中午上班啦!!!!!!!!!!!");
+    }
+
+    @Scheduled(cron="0 0 17 ? * *")
+    public void goHome() {
+        /*String time = new SimpleDateFormat("MMM d，yyyy KK:mm:ss a", Locale.ENGLISH).format(System.currentTimeMillis());*/
+        String time = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(System.currentTimeMillis());
+        System.out.println("time:"+time);
+        System.out.println("下班啦!!!!!!!!!!!");
+    }
+
+    @Scheduled(cron="0 10 18 ? * *")
+    public void goneHome() {
+        /*String time = new SimpleDateFormat("MMM d，yyyy KK:mm:ss a", Locale.ENGLISH).format(System.currentTimeMillis());*/
+        String time = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(System.currentTimeMillis());
+        System.out.println("time:"+time);
+        System.out.println("到家啦!!!!!!!!!!!");
+    }
+
 }
--- src/test/java/test/mytest.java
+++ src/test/java/test/mytest.java
@@ -6,9 +6,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class mytest {
 	public static void main(String[] args) {
 		System.out.println("TestQuartz start.");
-		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext-job.xml");
+		/*ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext-job.xml");*/
 		//如果配置文件中将startQuertz bean的lazy-init设置为false 则不用实例化
 		//context.getBean("taskManager");
+		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+
+		/*QuartzJob quartzJob=new QuartzJob();
+		quartzJob.work();*/
 		System.out.print("TestQuartz end..");
 	}
 }
