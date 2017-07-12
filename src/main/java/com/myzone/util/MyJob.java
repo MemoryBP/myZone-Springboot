72c1a6d86523b636688b1198407744f474f2b97c
memorybp <809558630@qq.com>
2017-07-12 11:11:47 +0800

    配置quartz

---
+++ src/main/java/com/myzone/util/MyJob.java
@@ -0,0 +1,12 @@
+package com.myzone.util;
+
+import java.util.Date;
+
+/**
+ * Created by 陈 on 2017/7/12.
+ */
+public class MyJob {
+    public void work() {
+        System.out.println("date:" + new Date().toString());
+    }
+}
