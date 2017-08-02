832a4cfe914653e10141cbf64a9f6575ac209c02
memorybp <809558630@qq.com>
2017-08-02 16:26:59 +0800

    增加跨域访问配置以及H5页面

--- src/main/java/com/myzonespringboot/MyzonespringbootApplication.java
+++ src/main/java/com/myzonespringboot/MyzonespringbootApplication.java
@@ -2,7 +2,12 @@ package com.myzonespringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -17,5 +22,38 @@ public class MyzonespringbootApplication extends WebMvcConfigurerAdapter {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/**").addResourceLocations("classpath:/static/").addResourceLocations("classpath:/templates/");
+	}
+
+
+	/*
+	 * 地址拦截器
+	 */
+	// 增加拦截器
+   /* @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(requestToken).addPathPatterns("/v1/**");
+    }*/
+	/*
+	 * 跨域请求配置
+	 */
+	private CorsConfiguration buildConfig() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.addAllowedOrigin("*"); // 1
+		corsConfiguration.addAllowedHeader("*"); // 2
+		corsConfiguration.addAllowedMethod("*"); // 3
+		return corsConfiguration;
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", buildConfig()); // 4
+		return new CorsFilter(source);
+	}
+
+	@Override
+	public void configurePathMatch(PathMatchConfigurer configurer) {
+		super.configurePathMatch(configurer);
+		configurer.setUseSuffixPatternMatch(false);
 	}
 }
